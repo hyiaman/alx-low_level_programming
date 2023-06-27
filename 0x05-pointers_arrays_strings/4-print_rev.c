4c8c436fb28543274d5113ef5f0205d126d910aa
hyiaman <hyiamosei@gmail.com>
2023-06-26 22:43:25 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,24 +3,24 @@
  *  * print_rev - imprime en reversa
  *   * @s: string
  *    * return: 0
- *     */
+ */
 void print_rev(char *s)
 {
-		int longi = 0;
+	int longi = 0;
 			int o;
 
-				while (*s != '\0')
-						{
-									longi++;
-											s++;
-												}
+			while (*s != '\0')
+			{
+				longi++;
+				s++;
+			}
+				s--;
+				for (o = longi; o > 0; o--)
+				{
+					_putchar(*s);
 					s--;
-						for (o = longi; o > 0; o--)
-								{
-											_putchar(*s);
-													s--;
-														}
+				}
 
-							_putchar('\n');
+			_putchar('\n');
 }
 
